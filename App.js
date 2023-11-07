c2907fbd5537ea73f8ad140fa1c6a0a32136b05f
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-11-08 01:29:23 +0500

    finished

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SplashScreen from 'react-native-splash-screen';
 import StackNavigation from './src/navigation/StackNavigation';
-
 const Stack = createNativeStackNavigator();
 function App() {
   useEffect(() => {
