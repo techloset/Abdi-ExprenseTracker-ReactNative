b8e707584a1713644079ff6e645b87d1026739a4
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-11-08 01:28:39 +0500

    finished

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React, {useEffect} from 'react';
-import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SplashScreen from 'react-native-splash-screen';
 import StackNavigation from './src/navigation/StackNavigation';
 
 const Stack = createNativeStackNavigator();
-
 function App() {
   useEffect(() => {
     SplashScreen.hide();
